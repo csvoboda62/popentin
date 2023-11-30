427dc156dcadd9ce39cb727d059be6b88e3b7d8c
Corentin <svoboda.corentin@gmail.com>
2023-11-30 23:42:28 +0100

    user admin

--- src/Controller/PopController.php
+++ src/Controller/PopController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/pop')]
+#[Route('/admin/pop')]
 class PopController extends AbstractController
 {
     #[Route('/', name: 'app_pop_index', methods: ['GET'])]
